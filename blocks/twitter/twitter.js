adce5db72e2670a2e7ac1c6a2c1bc7e521fa8ea5
Jeff Foxx <foxx@adobe.com>
2023-06-08 16:45:30 -0400

    delayed twitter

--- blocks/twitter/twitter.js
+++ blocks/twitter/twitter.js
@@ -1,21 +1,18 @@
 export default function decorate(block) {
   const cap = document.createElement('img');
   cap.src = '../images/twitter-cap.jpg';
+  cap.width = '338';
+  cap.height = '184';
   const wrapper = document.createElement('p');
   const timeline = document.createElement('a');
   timeline.className = 'twitter-timeline';
   timeline.setAttribute('data-aria-polite', 'assertive');
   timeline.setAttribute('data-chrome', 'noheader noscrollbar');
   timeline.setAttribute('data-height', '650');
-  timeline.setAttribute('data-width', '520');
+  timeline.setAttribute('data-width', '336');
   timeline.href = 'https://twitter.com/CanadianPM/lists/canada-live1';
   timeline.text = '';
-  const script = document.createElement('script');
-  script.setAttribute('async', '');
-  script.src = 'https://platform.twitter.com/widgets.js';
-  script.setAttribute('charset', 'utf-8');
   wrapper.append(timeline);
-  wrapper.append(script);
   block.textContent = '';
   block.append(cap);
   block.append(wrapper);
