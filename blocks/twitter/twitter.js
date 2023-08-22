21a61477e4ac44325790c0f92794c7f79ca329ea
Jeff Foxx <foxx@adobe.com>
2023-08-22 14:14:12 -0400

    removed twitter timeline

--- blocks/twitter/twitter.js
+++ blocks/twitter/twitter.js
@@ -4,15 +4,6 @@ export default function decorate(block) {
   cap.width = '338';
   cap.height = '184';
   const wrapper = document.createElement('p');
-  const timeline = document.createElement('a');
-  timeline.className = 'twitter-timeline';
-  timeline.setAttribute('data-aria-polite', 'assertive');
-  timeline.setAttribute('data-chrome', 'noheader noscrollbar');
-  timeline.setAttribute('data-height', '650');
-  timeline.setAttribute('data-width', '336');
-  timeline.href = 'https://twitter.com/CanadianPM/lists/canada-live1';
-  timeline.text = '';
-  wrapper.append(timeline);
   block.textContent = '';
   block.append(cap);
   block.append(wrapper);
